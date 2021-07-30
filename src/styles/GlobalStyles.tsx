70d51a70504bb51ebf3a606c7ea619c50b93e8b7
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2021-07-30 17:17:25 -0500

    global styles

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -4,20 +4,24 @@ export const GlobalStyles = createGlobalStyle`
   html {
     box-sizing: border-box;
     font-size: 16px;
-    --gray: #d4e3e6;
+    --blue: #DBE6FD;
     --gray-text:#536162;
     --white: #f4f9f9;
     --green: #c7ffd8;
     --shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
     --light-shadow: 0px 8px 16px 0px rgba(256, 256, 256, 0.2);
-    --red: #e84545;
+    --gray-rgba: rgba(246, 249, 249, 0.8);
     --loading-red: #c64756;
-    --blue: #1597bb;
-    --white-rgba: rgba(256, 256, 256, 0.9);
+    --fblue: #00308F;
+    --gray: #E4EFE7;
+    --white-rgba: rgba(256, 256, 256, 0.1);
     --content-width: 70vw;
     --content-height: 84vh;
     --top: 5vh;
-    --gap: 1vw;
+    --gap: 10px;
+    --padding: 10px;
+    --large-padding: 25px;
+    --radius: 3px;
   }
 
   *, *:before, *:after {
@@ -37,7 +41,7 @@ export const GlobalStyles = createGlobalStyle`
     margin: 0 auto;
     max-width: 100vw;
     overscroll-behavior: none;
-    background-color: rgb(255, 255, 255);
+    background-color: var(--gray);
     overflow-x: hidden;
     width: 100%; 
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
