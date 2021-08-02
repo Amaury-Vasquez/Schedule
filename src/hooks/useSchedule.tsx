d6a68f730134e7d898fe4f003053b830dca57267
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2021-08-02 14:47:57 -0500

    schedule update

--- src/hooks/useSchedule.tsx
+++ src/hooks/useSchedule.tsx
@@ -13,22 +13,22 @@ export const useSchedule = () => {
     {
       name: "Calculo Aplicado",
       hours: ["08:30-10:00", "", "08:30-10:00", "08:30-10:00", ""],
-      teacher: "",
+      teacher: "Viveros Vela Karina",
     },
     {
       name: "Mecanica y Electromagnetismo",
-      hours: ["10:30-12:00", "10:30-12:00", "", "10:30-12:00", "10:30-12:00"],
-      teacher: "",
+      hours: ["13:30-15:00", "12:00-13:30", "13:30-15:00", "", "12:00-13:30"],
+      teacher: "Cesar Hernandez Vasquez",
     },
     {
       name: "Ingenieria, Etica y Sociedad",
-      hours: ["12:00-13:30", "", "12:00-13:30", "12:00-13:30", ""],
-      teacher: "",
+      hours: ["20:00-21:30", "", "20:00-21:30", "20:00-21:30", ""],
+      teacher: "Hernandez Garcia Rosa Alba",
     },
     {
       name: "Fundamentos Economicos",
-      hours: ["13:30-15:00", "13:30-15:00", "", "13:30-15:00", ""],
-      teacher: "",
+      hours: ["12:00-13:30", "", "12:00-13:30", "12:00-13:30", ""],
+      teacher: "Castillo Marrufo Juan Antonio",
     },
     {
       name: "Algoritmos y Estructuras de datos",
@@ -37,7 +37,7 @@ export const useSchedule = () => {
     },
     {
       name: "Bases de Datos",
-      hours: ["", "15:00-16:30", "15:00-16:30", "15:00-16:30", ""],
+      hours: ["", "15:00-16:30", "15:00-16:30", "", "15:00-16:30"],
       teacher: "Botello Castillo Alejandro",
     },
   ]);
