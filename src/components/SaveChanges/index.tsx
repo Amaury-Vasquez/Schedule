b04d0070391ae242a4c17f947bfaab627ad0cc18
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2021-08-02 12:09:29 -0500

    Save changes menu

--- src/components/SaveChanges/index.tsx
+++ src/components/SaveChanges/index.tsx
@@ -2,5 +2,5 @@ import { FaRegSave } from "react-icons/fa";
 import { DropDownMenu } from "../DropDownMenu";
 
 export const SaveChanges = (props: { cb: Function }) => {
-  return <DropDownMenu callback={props.cb} cover={FaRegSave} />;
+  return <DropDownMenu callback={props.cb} Cover={FaRegSave} />;
 };
